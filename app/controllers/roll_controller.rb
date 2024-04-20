708bc6158c0009ad6279c4a610a7b61b364048e1
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-20 22:24:57 +0000

    first attempt: missing dynamic segments

---
+++ app/controllers/roll_controller.rb
@@ -0,0 +1,59 @@
+class RollController < ApplicationController
+  def homepage
+    render({ :template => "roll_templates/homepage" })
+  end
+
+  def two_six
+    @rolls = []
+
+    # Rolling the dice 2 times and storing each result
+    2.times do
+      die = rand(1..6)  # Includes 6 in the range
+      @rolls.push(die)
+    end
+
+    # Rendering the template once after all rolls are determined
+    render({ :template => "roll_templates/two_six" })
+  end
+
+def two_ten
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+    @rolls.push(die)
+  end
+
+   render({ :template => "roll_templates/two_ten" })
+   end
+
+  def one_twenty
+    @rolls = []
+    
+    1.times do
+      die = rand(1...20)
+      @rolls.push(die)
+  end
+  render({ :template => "roll_templates/one_twenty"})
+end
+
+def five_four
+  @rolls = []
+
+  5.times do
+    die = rand(1...4)
+    @rolls.push(die)
+  end
+  render({ :template => "roll_templates/five_four"})
+end
+
+def roll_dice
+  number_of_dice = params[:number_of_dice].to_i
+  number_of_sides = params[:number_of_sides].to_i
+  @rolls = Array.new(number_of_dice) { rand(1..number_of_sides) }
+
+  # This should match the name of your view template
+  render({ template: "roll_templates/roll_dice" })
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
 
+  get("/", {:controller => "roll", :action => "homepage"})
+
+  get("/dice/2/6", {:controller => "roll", :action => "two_six"})
+
+  get("/dice/2/10", {:controller => "roll", :action => "two_ten"})
+
+  get("/dice/1/20", {:controller => "roll", :action => "one_twenty"})
+
+  get("/dice/5/4", {:controller => "roll", :action => "five_four"})
+
+  get("/dice/:number_of_dice/:number_of_sides", { controller: "roll", action: "roll_dice" })
 end
